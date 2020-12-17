9af5ff7ba6fd35be7376d184ddbed2e79eb42bb8
zrr <zrexrodriguez@gmail.com>
2020-12-17 15:42:16 -0800

    feat: header css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function Header(props) {
   return (
     <nav className="header">
-      <h1 className="header__title"><a className="header__title-link">NewsExplorer</a></h1>
+      <a className="header__title-link"><h1 className="header__title">NewsExplorer</h1></a>
       <ul className="header__links">
         <li className="header__link header__link_selected">Home</li>
         <li className="header__link header__link_hidden">Saved Article</li>
-        <li className="header__link">
+        <li className="header__link header__link_button">
           <button className="header__login-button">Sign in</button>
         </li>
         <li className="header__link header__link_hidden">
