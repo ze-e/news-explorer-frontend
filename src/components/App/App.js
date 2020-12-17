9f8867b469d7bfb7a7ff85f07e685ee44c4c992f
zrr <zrexrodriguez@gmail.com>
2020-12-16 23:51:15 -0800

    feat: header css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 export default function App() {
   return (
     <div className="App">
-      Hello world!
+      <Header></Header>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,14 @@ import React from 'react';
 export default function Header(props) {
   return (
     <nav className="header">
-      <h1 className="header__title"><a className="header__title-link"></a></h1>
+      <h1 className="header__title"><a className="header__title-link">NewsExplorer</a></h1>
       <ul className="header__links">
-        <li className="header__link">Home</li>
-        <li className="header__link header__link_signed-in">Saved Article</li>
-        <li className="header__link header__link_signed-out">
+        <li className="header__link header__link_selected">Home</li>
+        <li className="header__link header__link_hidden">Saved Article</li>
+        <li className="header__link">
           <button className="header__login-button">Sign in</button>
         </li>
-        <li className="header__link header__link_signed-in">
+        <li className="header__link header__link_hidden">
           <button className="header__profile-button">myName<span className="header__profile-button-icon">-</span></button>
         </li>
       </ul>
