9efd61aeda2216c369dcab27c73252b814b07af2
zrr <zrexrodriguez@gmail.com>
2020-12-21 14:14:13 -0800

    css header logged out

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,16 +6,16 @@ export default function Header(props) {
     <nav className="header">
       <a className="header__title-link"><h1 className="header__title">NewsExplorer</h1></a>
       <ul className="header__links">
-        <li className="header__link header__link_selected">Home</li>
-        <li className="header__link header__link_hidden">Saved Article</li>
+        <li className="header__link">Home</li>
+        <li className="header__link header__link_selected">Saved Article</li>
         <li className="header__link header__link_button">
-          <button className="header__login-button">Sign in</button>
-        </li>
-        <li className="header__link header__link_hidden">
-          <button className="header__profile-button">myName<span className="header__profile-button-icon">-</span></button>
+          <button className="header__login-button header__link_hidden">Sign in</button>
         </li>
         <li className="header__link">
-          <button className="header__collapse-button">--</button>
+          <button className="header__profile-button">myName<span className="header__profile-button-icon"></span></button>
+        </li>
+        <li className="header__link">
+          <button className="header__collapse-button"></button>
         </li>
       </ul>
     </nav>
