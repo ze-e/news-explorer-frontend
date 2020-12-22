e46324ac8ed42ed089a1916d1350552427e4e379
zrr <zrexrodriguez@gmail.com>
2020-12-21 16:11:56 -0800

    css header logged in

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,16 +3,16 @@ import React from 'react';
 export default function Header(props) {
   return (
     <>
-    <nav className="header">
+    <nav className="header header__logged_in">
       <a className="header__title-link"><h1 className="header__title">NewsExplorer</h1></a>
       <ul className="header__links">
         <li className="header__link">Home</li>
-        <li className="header__link header__link_selected">Saved Article</li>
+        <li className="header__link header__link_selected header__logged_in">Saved Article</li>
         <li className="header__link header__link_button">
-          <button className="header__login-button header__link_hidden">Sign in</button>
+          <button className="header__button header__link_hidden">Sign in</button>
         </li>
-        <li className="header__link">
-          <button className="header__profile-button">myName<span className="header__profile-button-icon"></span></button>
+        <li className="header__link header__link_button">
+          <button className="header__button header__logged_in">myName<div className="header__button-icon"></div></button>
         </li>
         <li className="header__link">
           <button className="header__collapse-button"></button>
