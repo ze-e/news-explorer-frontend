81061cd010a497eaee1e3c2f688b297767aa4cf1
zrr <zrexrodriguez@gmail.com>
2020-12-21 20:05:01 -0800

    css header logged in responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,12 +7,12 @@ export default function Header(props) {
       <a className="header__title-link"><h1 className="header__title">NewsExplorer</h1></a>
       <ul className="header__links">
         <li className="header__link">Home</li>
-        <li className="header__link header__link_selected header__logged_in">Saved Article</li>
-        <li className="header__link header__link_button">
-          <button className="header__button header__link_hidden">Sign in</button>
+        <li className="header__link header__link_selected">Saved Article</li>
+        <li className="header__link header__link_button header__link_hidden">
+          <button className="header__button">Sign in</button>
         </li>
         <li className="header__link header__link_button">
-          <button className="header__button header__logged_in">myName<div className="header__button-icon"></div></button>
+          <button className="header__button">myName<div className="header__button-icon"></div></button>
         </li>
         <li className="header__link">
           <button className="header__collapse-button"></button>
