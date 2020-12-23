76324b21e1e445a1c6590cd30590ba6b83cfeb5e
zrr <zrexrodriguez@gmail.com>
2020-12-22 22:36:25 -0800

    main-about loggedin responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,12 +6,12 @@ export default function Header(props) {
     <nav className="header">
       <a className="header__title-link"><h1 className="header__title">NewsExplorer</h1></a>
       <ul className="header__links">
-        <li className="header__link header__link_selected">Home</li>
-        <li className="header__link header__link_selected header__link_hidden">Saved Article</li>
-        <li className="header__link header__link_button">
+        <li className="header__link">Home</li>
+        <li className="header__link header__link_selected">Saved Article</li>
+        <li className="header__link header__link_button header__link_hidden">
           <button className="header__button">Sign in</button>
         </li>
-        <li className="header__link header__link_button header__link_hidden">
+        <li className="header__link header__link_button">
           <button className="header__button">myName<div className="header__button-icon"></div></button>
         </li>
         <li className="header__link">
