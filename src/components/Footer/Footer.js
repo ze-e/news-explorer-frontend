b8fd4615bb7c29484cec669eff877e1782d398d8
zrr <zrexrodriguez@gmail.com>
2020-12-31 17:09:10 -0800

    css footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,12 +4,16 @@ export default function Footer() {
   return (
     <section className="footer">
       <p className="footer__copyright">© 2020 Supersite, Powered by News API</p>
-      <ul className="footer__nav">
-        <li className="footer__nav-item"><a className="footer__nav-link">Home</a></li>
-        <li className="footer__nav-item"><a className="footer__nav-link">Practicum by Yandex</a></li>
-        <li className="footer__nav-item"><button className="footer__nav-button footer__nav-button_facebook"></button></li>
-        <li className="footer__nav-item"><button className="footer__nav-button footer__nav-button_github"></button></li>
-      </ul>
+      <div className="footer__links">
+        <ul className="footer__nav">
+          <li className="footer__nav-item"><a className="footer__nav-link">Home</a></li>
+          <li className="footer__nav-item"><a className="footer__nav-link">Practicum by Yandex</a></li>
+        </ul>
+        <ul className="footer__social">
+          <li className="footer__social-item"><button className="footer__social-button footer__social-button_facebook"></button></li>
+          <li className="footer__social-item"><button className="footer__social-button footer__nav-button_github"></button></li>
+        </ul>
+      </div>
     </section>
   );
 }
