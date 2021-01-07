f2ed9a94464b52d8726147921c7610f42e56058b
zrr <zrexrodriguez@gmail.com>
2021-01-06 22:37:51 -0800

    css signed in styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,8 +7,8 @@ export default function Header(props) {
     <nav className="header">
       <Link className="header__title-link" to="/"><h1 className="header__title">NewsExplorer</h1></Link>
       <ul className="header__links">
-      <li className="header__item header__link_selected"><Link className="header__link" to="/">Home</Link></li>
-      <li className={`header__item ${!props.loggedIn && 'header__link_selected header__link_hidden'}`}><Link className="header__link" to="/saved-news">Saved Article</Link></li>
+      <li className="header__item"><Link className="header__link" to="/">Home</Link></li>
+      <li className={`header__item header__link_selected ${!props.loggedIn && 'header__link_hidden'}`}><Link className="header__link" to="/saved-news">Saved Article</Link></li>
         <li className={`header__item header__link_button ${props.loggedIn &&'header__link_hidden'}`}>
           <button className="header__button" type="button" onClick={props.onOpen}>Sign in</button>
         </li>
