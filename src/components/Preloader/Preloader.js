f2ec7432972dd4c0074da3b8d8e4169d4542bfa2
zrr <zrexrodriguez@gmail.com>
2021-01-08 14:27:25 -0800

    added css to preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Preloader() {
   return (
-    <section className="preloader"><p>Loading...</p></section>
+    <section className="preloader"><p>Searching for news...</p></section>
   ) 
 }
