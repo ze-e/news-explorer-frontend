c7f72af076ec2a3263950ef5354d3fd0ff65effa
zrr <zrexrodriguez@gmail.com>
2021-02-04 20:45:55 -0800

    fixing grid

--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -6,8 +6,8 @@ import NewsCardList from '../NewsCardList/NewsCardList';
 export default function SavedNews(props) {
   return (
     <div className="savedNews">
-      <h2 className="savedNews__title">Search Results</h2>
       <SavedNewsHeader />
+      <h2 className="savedNews__title">Search Results</h2>
       <NewsCardList isSignedIn={props.isSignedIn}/>
     </div>
   );
