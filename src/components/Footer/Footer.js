ac141e6d05e644b9283951267d763ba9331dc81d
zrr <zrexrodriguez@gmail.com>
2021-02-06 16:09:27 -0800

    fixed margins footer about

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,16 +5,19 @@ export default function Footer() {
   return (
     <section className="footer">
       <p className="footer__copyright">© 2020 Supersite, Powered by News API</p>
-      <div className="footer__links">
+      
+      <div className="footer__links">     
         <ul className="footer__nav">
           <li className="footer__nav-item"><NavLink to="/" className="footer__nav-link">Home</NavLink></li>
           <li className="footer__nav-item"><a href="https://practicum.yandex.com/" target="_blank" className="footer__nav-link">Practicum by Yandex</a></li>
         </ul>
+        
         <ul className="footer__social">
           <li className="footer__social-item"><a href="https://github.com/ze-e/news-explorer-frontend" target="_blank"><div className="footer__social-button footer__social-button_github"></div></a></li>
           <li className="footer__social-item"><a href="https://www.facebook.com/" target="_blank"><div className="footer__social-button footer__social-button_facebook"></div></a></li>
-        </ul>
+        </ul> 
       </div>
+
     </section>
   );
 }
