9323ffe8adae791a25ca84fbb08c0aee53d5e1be
zrr <zrexrodriguez@gmail.com>
2021-02-15 21:58:00 -0800

    changed news endpoint to everything

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 exports.baseURL = 'https://news-explorer-server.herokuapp.com/api';
 exports.APIkey = 'de0e72f1af3b4dbfbdd6bc7e55939745';
-exports.proxyURL='https://nomoreparties.co/news/v2/everything?country=us&apiKey=';
+exports.proxyURL='https://nomoreparties.co/news/v2/everything?apiKey=';
