7b458568710f3cbb7c5dc162c09d6fc655dd6eb9
zrr <zrexrodriguez@gmail.com>
2021-02-19 15:18:09 -0800

    fixed header display name

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -3,7 +3,7 @@ import {CurrentCardsContext} from '../../contexts/CurrentCardsContext';
 import {CurrentUserContext} from '../../contexts/CurrentUserContext';
 
 export default function SavedNewsHeader(props) {
-  const username = React.useContext(CurrentUserContext);
+  const User = React.useContext(CurrentUserContext);
   const cards = React.useContext(CurrentCardsContext);
   const keywords = commonKeyword(cards.map(card => card.keyword));
   
@@ -17,7 +17,7 @@ export default function SavedNewsHeader(props) {
   return (
     <section className="SavedNewsHeader">
       <p className="SavedNewsHeader__caption">Saved articles</p>
-      <h2 className="SavedNewsHeader__title">{username ? `username, you`: "you"} have {cards.length} saved articles</h2>
+      <h2 className="SavedNewsHeader__title">{User ? `${User.name}, you`: "you"} have {cards.length} saved articles</h2>
       <h3 className="SavedNewsHeader__keywords-title">{keywords.length > 0 && "By Keywords: "} 
         <span className="SavedNewsHeader__keywords-list"> 
           {keywords[0] && `${keywords[0]}`} 
